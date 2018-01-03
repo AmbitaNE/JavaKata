9932b59c3ac52694e5f227dceb8e2858b7aee014
Erik Germundson <ege@ambita.com>
2018-01-03 12:31:06 +0100

    upgrade to junit 5

--- kata-tdd-common/src/test/java/com/ambita/kata/common/StringToNumbersTest.java
+++ kata-tdd-common/src/test/java/com/ambita/kata/common/StringToNumbersTest.java
@@ -1,53 +1,65 @@
 package com.ambita.kata.common;
 
-import org.junit.Test;
+import java.math.BigInteger;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StringToNumbersTest {
 
+  private static final int EXPECTED_RESULT_ZERO = BigInteger.ZERO.intValue();
+  private static final int EXECPTED_RESULT_ONE = BigInteger.ONE.intValue();
+  private static final int EXECPTED_RESULT_MINUS_ONE = BigInteger.ONE.negate().intValue();
+
+  @DisplayName("Test null input")
   @Test
   public void testIsEmpty_nullIn() {
 
     assertTrue(new StringToNumbers().isEmpty(null));
   }
 
+  @DisplayName("Test empty input")
   @Test
   public void testIsEmpty_emptyIn() {
 
     assertTrue(new StringToNumbers().isEmpty(""));
   }
 
+  @DisplayName("Test input with content")
   @Test
   public void testIsEmpty_stringIn() {
 
     assertFalse(new StringToNumbers().isEmpty("-1"));
   }
 
-  @Test
-  public void testParseNumber_NullIn() {
-    assertEquals(Integer.valueOf(0), new StringToNumbers().parseNumber(null));
+  @DisplayName("Test input that should result in zero")
+  @ParameterizedTest
+  @ValueSource(strings = {  "","D" })
+  public void testParseNumber_ExpectedZero(String value) {
+    assertEquals(Integer.valueOf(EXPECTED_RESULT_ZERO), new StringToNumbers().parseNumber(value));
   }
 
+  @DisplayName("Test input Null that should result in zero")
   @Test
-  public void testParseNumber_emptyIn() {
-    assertEquals(Integer.valueOf(0), new StringToNumbers().parseNumber(""));
+  public void testParseNumber_NullInExpectedZero() {
+    assertEquals(Integer.valueOf(EXPECTED_RESULT_ZERO), new StringToNumbers().parseNumber(null));
   }
 
-  @Test
-  public void testParseNumber_letterIn() {
-    assertEquals(Integer.valueOf(0), new StringToNumbers().parseNumber("D"));
-  }
-
+  @DisplayName("Test input that should result in positive number")
   @Test
   public void testParseNumber_positivIn() {
-    assertEquals(Integer.valueOf(1), new StringToNumbers().parseNumber("1"));
+    assertEquals(Integer.valueOf(EXECPTED_RESULT_ONE), new StringToNumbers().parseNumber("1"));
   }
 
+  @DisplayName("Test input that should result in negative number")
   @Test
   public void testParseNumber_negativeIn() {
-    assertEquals(Integer.valueOf(-1), new StringToNumbers().parseNumber("-1"));
+    assertEquals(Integer.valueOf(EXECPTED_RESULT_MINUS_ONE), new StringToNumbers().parseNumber("-1"));
   }
 }
